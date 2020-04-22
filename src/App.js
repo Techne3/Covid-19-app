531b90a796e1f74c4a0aef53244f71e5da6b9066
Timothy Sharkey <Techne3@gmail.com>
2020-04-21 18:10:19 -0600

    components layout structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./app.module.css";
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>App</h1>
+        <Cards />
+        <CountryPicker />
+        <Chart />
       </div>
     );
   }
