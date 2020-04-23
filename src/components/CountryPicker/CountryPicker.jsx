a6f461e93a2ca7aec9e8cba4c91cd8903fdd00d0
Timothy Sharkey <Techne3@gmail.com>
2020-04-22 20:06:01 -0600

    Created drop down menu for all  countries in api. Styled the chart

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { NativeSelect, FormControl } from "@material-ui/core";
+import styles from "./CountryPicker";
+import { fetchCountries } from "../../api/index";
 
 const CountryPicker = () => {
-  return <h1>CountryPicker</h1>;
+  const [fetchedCountries, setFetchedCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setFetchedCountries(await fetchCountries());
+    };
+    fetchAPI();
+  }, [setFetchedCountries]);
+
+  console.log(fetchedCountries);
+
+  return (
+    <FormControl className={styles.formControl}>
+      <NativeSelect>
+        <option value="global">Global</option>
+        {fetchedCountries.map((country, i) => (
+          <option key={i} value={country}>
+            {country}
+          </option>
+        ))}
+      </NativeSelect>
+    </FormControl>
+  );
 };
 
 export default CountryPicker;
